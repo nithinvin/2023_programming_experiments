fe837b60cf3f979fe55035e7b8da551536306c4f
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2023-03-24 19:21:23 +0530

    Add source files to add two numbers in C Language and CPP Language

---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b = 7, c = 9;
+    a = b + c;
+
+    printf("%d\n", a);
+}
---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a, b = 7, c = 9;
+
+   a = b + c;
+   cout << a << endl;
+}
